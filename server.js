f797defaeff760f3025e9c70df0b3f9c052b6e23
eccpluto <alleyes.payday591@passmail.net>
2024-06-30 13:27:08 +1200

    npm project setup, server has template code

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+// require("dotenv").config();
+
+// // pull in database setup code
+// let dbConnect = require('./dbConnect');
+
+// // pull in routes
+// let userRoutes = require('./routes/userRoutes');
+// app.use('/api/users', userRoutes);
+
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//     res.json({ messsage: "Welcome to my MongoDB application." })
+// });
+
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => {
+//     console.log(`Server is listening on port ${PORT}`);
+// });
