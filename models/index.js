69ad4988de6595696fc79a57116cc003b5512c0e
eccpluto <alleyes.payday591@passmail.net>
2024-06-30 17:18:37 +1200

    basic minion shema outline, will be used as initial testing collection

---
+++ models/index.js
@@ -0,0 +1,4 @@
+'use strict';
+module.exports = {
+    minion: require('./minion')
+};
---
+++ models/minion.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const minionSchema = new Schema({
+    // id is number of entities in collection-1, would be good to auto-index this,
+    // otherwise just use default ObjectId string for local commits to MongoDB 
+    id: { type: Number, trim: true, required: true, unique: true },
+    name: { type: String, trim: true, required: true },
+    description: { type: String, trim: true },
+    enhanced_description: { type: String, trim: true },
+    toolTip: {},
+    patch: {},
+    // can be null?
+    item_id: {},
+    tradeable: { type: Boolean },
+    // should be a FK to behaviours collection
+    behaviour: {},
+    // should be a FK to races collection
+    race: {},
+    image: { type: String },
+    icon: { type: String },
+    owned: { type: String },
+    // array of FKs to different sources
+    sources: { type: Array },
+    // there is a skill_type in here that should be a FK to the skill_types collection
+    verminion: { type: Object },
+});
+
+module.exports = mongoose.model("minion", minionSchema);
